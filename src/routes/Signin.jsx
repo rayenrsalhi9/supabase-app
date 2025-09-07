60920867302f92af32e23bbd65af11fdae71ee0e
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-09-07 22:29:23 +0100

    added the signin component (static)

--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 export default function Signin() {
   return (
-    <h1>Signin page goes here</h1>
+    <form className="form">
+      <h2>Welcome back!</h2>
+      <span className="input-span">
+        <label htmlFor="email" className="label">Email</label>
+        <input type="email" name="email" id="email" placeholder="example@domain.com" />
+      </span>
+      <span className="input-span">
+        <label htmlFor="password" className="label">Password</label>
+        <input type="password" name="password" id="password" placeholder="********" />
+      </span>
+      <button className="submit-btn">Sign in</button>
+      <span className="span">Don't have an account? <Link to="/signup">Sign up</Link></span>
+    </form>
   )
 }
