ef79960a4401fdf9d99aeb04fc2f7f4d860bb14c
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-09-09 11:31:41 +0100

    feat(header): add user section with sign out button
    Add react-icons dependency and implement user section in header component to display user email and sign out button. Style the header to accommodate new elements with flex layout.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,28 @@
+import { useAuth } from "../context/AuthContext"
+import { VscSignOut } from "react-icons/vsc";
+
 export default function Header() {
+
+    const {session} = useAuth()
+
     return(
         <header className='dashboard-header'>
             <h1>Sales Dashboard</h1>
+            <div className="user-section">
+                {
+                    session?.user?.email
+                    ? (
+                        <>
+                            <p>{session.user.email}</p>
+                            <button>
+                                <VscSignOut />
+                                Sign out
+                            </button>
+                        </>
+                    )
+                    : null
+                }
+            </div>
         </header>
     )
 }
